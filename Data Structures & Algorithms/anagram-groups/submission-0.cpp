b951244ad525f539a70e9c6e44346e3540c1e585
Henrique Stelzer de Oliveira <henrique.stelzer@tuta.io>
2026-06-21 12:10:54 -0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<int, vector<int>> m;
+        set<int> s;
+        for (int i = 0; i < strs.size(); i++) {
+            int hash = 1;
+            for (auto& jt : strs[i]) hash *= jt;
+            hash = hash % 1000000007;
+            m[hash].push_back(i);
+            s.insert(hash);
+        }
+
+        vector<vector<string> > awns(m.size(), vector<string> {});
+        int i = 0;
+        for (auto it : s) {
+            for (auto& jt : m[it]) awns[i].push_back(strs[jt]);
+            i++;
+        }
+
+        return awns;
+    }
+};
