f06f837ce1dc432b44170f68bdcf5737ada4238d
Henrique Stelzer de Oliveira <henrique.stelzer@tuta.io>
2026-07-18 11:30:27 -0300

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        set<long long> vis;
+        vector<vector<int>> aws;
+        for (int i = 1; i < nums.size()-1; i++) {
+            int j = 0, k = nums.size()-1;
+            int target = -1 * nums[i];
+            while (j < i && k > i) {
+                if (nums[j] + nums[k] > target) k--;
+                else if (nums[j] + nums[k] < target) j++;
+                else {
+                    if (!vis.contains(nums[i] + nums[j]*1e5 + nums[k]*1e10)) {
+                        aws.push_back(vector<int> {nums[i], nums[j], nums[k]});
+                        vis.insert(nums[i] + nums[j]*1e5 + nums[k]*1e10);
+                    }
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return aws;
+    }
+};
