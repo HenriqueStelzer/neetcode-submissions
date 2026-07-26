131f1cab87c3461da79adeb63f8608d9753d6df6
Henrique Stelzer de Oliveira <henrique.stelzer@tuta.io>
2026-07-25 23:28:25 -0300

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char, int> start;
+        int mx=0, i=0;
+        for (int j = 0; j < s.size(); j++) {
+            if (start[s[j]]) {
+                i = max(i, start[s[j]]);
+            }
+            start[s[j]] = j + 1;
+            mx = max(mx, j-i+1);
+        }
+        return mx;
+    }
+};
