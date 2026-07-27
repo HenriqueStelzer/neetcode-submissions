6b2402a547cefb042a3bbb378bd3cf1619da4db0
Henrique Stelzer de Oliveira <henrique.stelzer@tuta.io>
2026-07-27 15:51:51 -0300

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int cur=0, sum=0, res=-1;
+        for (int i = 0; i < gas.size(); i++) {
+            cur = gas[i] - cost[i];
+            sum += cur;
+            if (res == -1) {
+                if (cur >= 0) res = i;
+            }
+            else if (sum < 0) res = -1;
+            cout << sum << ' ' << cur << ' ' << res << '\n';
+        }
+        if (sum < 0) return -1;
+        return res; 
+    }
+};
