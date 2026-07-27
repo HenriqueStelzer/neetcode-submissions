a9e4c212861bf0e7410e8aa5ca7fcd4ade3b1ba6
Henrique Stelzer de Oliveira <henrique.stelzer@tuta.io>
2026-07-27 16:21:45 -0300

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        array<int8_t, 1001> count{};
+        for (int i = 0; i < hand.size(); i++) count[hand[i]]++;
+        int sum = 0;
+        for (int i = 0; i <= 1000; i++) {
+            if (count[i] > 0) {
+                if (i+groupSize >= 1000) return false;
+                int x = count[i];
+                for (int j = i; j < i+groupSize; j++) {
+                    count[j] -= x;
+                }
+            }
+            sum += count[i];
+        }
+        return (sum == 0);
+    }
+};
