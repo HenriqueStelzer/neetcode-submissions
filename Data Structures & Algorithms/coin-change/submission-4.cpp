f0ad9936d857d0c172bacfb4f27fe9596147956c
Henrique Stelzer de Oliveira <henrique.stelzer@tuta.io>
2026-08-19 16:35:45 -0300

    Add: coin-change - submission-4

---
+++ Data Structures & Algorithms/coin-change/submission-4.cpp
@@ -0,0 +1,23 @@
+constexpr int INF = 1e9+7;
+constexpr int MAX = 10 + 1;
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int res = dp(coins, amount, coins.size()-1);
+        if (res == INF) return -1;
+        return res;
+    }
+
+private:
+    array<unordered_map<int, int>, MAX> memo;
+    int dp(vector<int>& coins, int target, int top) {
+        if (target == 0) return 0;
+        if (memo[top][target] != 0) return memo[top][target];
+        int mn = INF;
+        for (int i = 0; i <= top; i++) {
+            if (target < coins[i]) continue;
+            mn = min(mn, dp(coins, target - coins[i], i) + 1);
+        }
+        return memo[top][target] = mn;
+    }
+};
